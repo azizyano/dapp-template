221327fd652be27f359fe077c411b576e1d2c611
nicholas <nicholasangks@gmail.com>
2021-07-26 03:34:37 +0800

    Add files via upload

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -7,20 +7,8 @@ contract Transaction {
     uint256 private count = 0;
     uint256 moneyStored = 0;
 
-    function incrementCounter() public {
-        count += 1;
-    }
-
-    function decrementCounter() public {
-        count -= 1;
-    }
-
     function addMoney() public payable {
         moneyStored += msg.value;
-    }
-
-    function getCount() public view returns (uint256) {
-        return count;
     }
 
     function getMoneyStored() public view returns (uint256) {
